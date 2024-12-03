c7d12650d59b7dc43fb63f8e2545fe703e9565ee
Nathasha Gama <nathashagama@gmail.com>
2024-12-02 21:19:08 -0300

    Quase Finalizado, Falta checar se todos os requisitos foram alcançados

--- ListarOrdemInversa.mongodb.js
+++ ListarOrdemInversa.mongodb.js
@@ -3,3 +3,7 @@ const database = 'Chat';
 const collection = 'Conversas'
 
 use(database);
+
+db[collection].find({
+    
+}).sort({DataHora: -1});
--- pesquisaPorPalavraExpecifica.mongodb.js
+++ pesquisaPorPalavraExpecifica.mongodb.js
@@ -4,4 +4,7 @@ const collection = 'Conversas'
 
 use(database);
 
-db[collection].find()
+db[collection].find(
+    {"conversa":/farmácias/i},
+
+)
